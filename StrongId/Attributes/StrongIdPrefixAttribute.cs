4821225f7feb4ad8b91eed929e35f80ca80063ba
Rafael Moreira <rafaelmoreira@Rafaels-Work-MacBook-Pro.local>
2026-05-06 11:08:29 -0300

    feat: Add AttributeUsage to StrongIdPrefixAttribute for class-level usage

--- StrongId/Attributes/StrongIdPrefixAttribute.cs
+++ StrongId/Attributes/StrongIdPrefixAttribute.cs
@@ -1,5 +1,6 @@
 namespace StrongId.Attributes;
 
+[AttributeUsage(AttributeTargets.Class)]
 public class StrongIdPrefixAttribute(string prefix) : Attribute
 {
     public string Prefix { get; } = prefix;
